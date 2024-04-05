75b5a3edb9c709f391428bc9015c28fe7125fd94
LerieLogin <161064384+LerieLogin@users.noreply.github.com>
2024-04-04 21:02:43 -0500

    parse query params and handles no searchQuery

--- js/search-results.js
+++ js/search-results.js
@@ -1,3 +1,5 @@
+const contaierEL = document.querySelector('main')
+
 function locSearch(query, format="search"){
     fetch(`https://www.loc.gov/${format}/?q=${query}&fo=json`)
     .then(function(response){
@@ -9,7 +11,24 @@ function locSearch(query, format="search"){
     })
 } 
 
+function errorMsg(error) {
+    const alert = document.createElement('div')
+    alert.classList.add('alert', 'alert-danger')
+    alert.innerText = error
+    contaierEL.appendChild(alert)
+}
 
-locSearch('dogs')
-locSearch('kittens','photos')
-locSearch('baseball','web-archives')
+function init() {
+    const searchParams = new URLSearchParams(location.search)
+
+    const searchQuery = searchParams.get('searchQuery')
+    const format = searchParams.get('format')
+
+    if (!searchQuery) {
+        errorMsg('no info provided') // make function with for this error if wanted
+    }
+    
+}
+
+init()
+
